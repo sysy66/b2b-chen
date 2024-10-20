d2697d1e61a890d85e06503487e0720f2b309afd
caolv <shaoyinshaoyang@gmail.com>
2024-10-20 19:18:01 +0800

    Functional test now checks we can see details of a product.

--- src/function_tests/base.py
+++ src/function_tests/base.py
@@ -1,5 +1,9 @@
 import unittest
+from datetime import time
+
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,20 +20,29 @@ class NewVisitorTest(unittest.TestCase):
         
         # She notices the page title and header mention WUHAN STARSTONE
         self.assertIn("WUHAN STARSTONE", self.browser.title)
-        
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("WUHAN STARSTONE", header_text)
         
         # She noticed a product code named G602
+        product_info = self.browser.find_element(By.TAG_NAME, "h3")
+        self.assertEqual(product_info.text, "G602")
         
         # She clicked on the product image
+        product_info.send_keys(Keys.ENTER)
+        time.sleep(1)
         
         # When she hits enter, the page updates, and now the products details
+        gallery = self.browser.find_element(By.CLASS_NAME, "gallery")
+        img = gallery.find_element(By.TAG_NAME, "img")
+        self.assertTrue(img.get_attribute("alt"), "G602")
         
         # She noticed that there was a window on the page for submitting information
         
         # She submitted her own information
         
         # Satisfied, she goes back to sleep
+        self.fail("Finish the test!")
+
 
 if __name__ == "__main__":
     unittest.main()
