49a814e0055e7e6b35480c1726d79f1eefacb814
caolv <shaoyinshaoyang@gmail.com>
2024-10-20 16:21:52 +0800

    First unit test and view function. url config, map / to home_page view.

--- src/products/tests.py
+++ src/products/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from products.views import home_page
 
 
-class SmokerTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
-        
+class HomePageTest(TestCase):
+    def test_home_page_return_correct_html(self):
+        response = self.client.get("/")
+        html = response.content.decode('utf-8')
+        self.assertIn("<title>WUHAN STARSTONE</title>", html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("</html>"))
