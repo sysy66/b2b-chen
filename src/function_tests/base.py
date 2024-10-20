f63a6d263a38e73473c750c964a1063849ff1ddc
caolv <shaoyinshaoyang@gmail.com>
2024-10-20 15:39:08 +0800

    FT specced out in comments, and now uses unittest.

--- src/function_tests/base.py
+++ src/function_tests/base.py
@@ -1,4 +1,35 @@
+import unittest
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Congratulations!' in browser.title
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_browse_a_product_page_and_submit_message(self):
+        # Edith has heard about a cool new online website WUHAN STARSTONE.
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8811/products")
+        
+        # She notices the page title and header mention WUHAN STARSTONE
+        self.assertIn("WUHAN STARSTONE", self.browser.title)
+        
+        self.fail("Finish the test!")
+        
+        # She noticed a product code named G602
+        
+        # She clicked on the product image
+        
+        # When she hits enter, the page updates, and now the products details
+        
+        # She noticed that there was a window on the page for submitting information
+        
+        # She submitted her own information
+        
+        # Satisfied, she goes back to sleep
+
+if __name__ == "__main__":
+    unittest.main()
