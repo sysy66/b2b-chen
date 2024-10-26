a21d7e2452c53612a9d4385f270e8d54cee1f4c4
caolv <shaoyinshaoyang@gmail.com>
2024-10-26 19:31:05 +0800

    some work for deployment

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:slim
+
+RUN python -m venv /venv
+ENV PATH="/venv/bin:$PATH"
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY src /src
+
+WORKDIR /src
+
+CMD gunicorn --bind :8888 b2b.wsgi:application
--- src/b2b/settings.py
+++ src/b2b/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ydecm!p@j6#d$5*+)=zlfd-8mkgw3t379g)51eg4g3*4wvrhct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if "DJANGO_DEBUG_FALSE" in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    ALLOWED_HOSTS = [os.environ["DJANGO_ALLOWED_HOST"]]
+else:
+    DEBUG = True
+    SECRET_KEY = "insecure-key-for-dev"
+    ALLOWED_HOSTS = []
 
 
 # Application definition
