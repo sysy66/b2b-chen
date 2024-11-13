2bf9477d8cb63d85ea34b6cd6cb284b47ebf7a8a
caolv <shaoyinshaoyang@gmail.com>
2024-11-14 01:50:16 +0800

    register PageInfo to admin

--- src/about_us/admin.py
+++ src/about_us/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PageInfo
 
-# Register your models here.
+
+class PageInfoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'desc', 'img', 'is_active']}),
+        ]
+    list_display = ['title', 'desc', 'img', 'is_active']
+    list_filter = ['title', 'is_active']
+    search_fields = ['title', ]
+
+
+admin.site.register(PageInfo, PageInfoAdmin)
